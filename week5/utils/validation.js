f7fa58bfb354e270a173c96f97cc3a1b094d1114
YYL0911 <94774269+YYL0911@users.noreply.github.com>
2025-02-20 14:27:46 +0800

    Update validation.js

--- week5/utils/validation.js
+++ week5/utils/validation.js
@@ -2,12 +2,12 @@ function isUndefined (value) {
   return value === undefined
 }
 
-function isNotValidString (value) {
-  return typeof value !== "string" || decodeURIComponent(value).trim().length === 0
+function isNotValidSting (value) {
+  return typeof value !== 'string' || value.trim().length === 0 || value === ''
 }
 
 function isNotValidInteger (value) {
-  return typeof value !== "number" || value < 0 || value % 1 !== 0
+  return typeof value !== 'number' || value < 0 || value % 1 !== 0
 }
 
 function isNotValidUuid(value){
